8f9eba19aa660696c9dc613671e23da71ff5a29e
FrancoisBasset <mrfrancoisbasset@gmail.com>
2026-05-31 18:38:24 +0200

    reset button

--- chochodile.py
+++ chochodile.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
+import shutil
 import subprocess
+
+def reset():
+    subprocess.run(["clear"])
+    terminal_width: int = shutil.get_terminal_size().columns
+
+    for _ in range(terminal_width):
+        print('█', end='')
+
+    print()
+
+    start: int = terminal_width // 2 - 4
+
+    for _ in range(start):
+        print('█', end='')
+
+    print('Sevenux', end='')
+
+    for _ in range(start + 7, terminal_width):
+        print('█', end='')
+
+    print()
+
+    for _ in range(terminal_width):
+        print('█', end='')
+
+    print('q pour quitter, r pour reset')
+
+reset()
 
 text: str = ''
 
 while True:
-    text: str = input('Calcul (q pour quitter) : ')
+    text: str = input('Calcul : ')
 
     if text == 'q':
         break
+    if text == 'r':
+        reset()
+        continue
 
     try:
         print(eval(text))
@@ -16,3 +48,4 @@ while True:
         pass
 
 subprocess.run(['poweroff'])
+
